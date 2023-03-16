935a0467110f182c009d14485bee8f80b65b470a
yauyauua <mahathir@students.amikom.ac.id>
2023-03-16 14:54:18 +0700

    Nambah fungsi penambahan dan pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -1,1 +1,22 @@
-y
+class calculator
+{
+	static void Main(string[] args)
+	{
+		
+	}
+	{
+		static int Penambahan(int a, int b)
+	}
+	
+	{
+		retyrn a+ b;
+	}
+	
+	{
+		static imt Pengurangan(int a, int b)
+	}
+	
+	{
+		return a - b;
+	}
+}
